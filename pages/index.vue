04704103cd9a5369f1e2d854103969b898783a13
aguileraq <aguileraq@users.noreply.github.com>
2023-02-08 00:49:55 -0600

    post signup

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,42 @@
 <template>
-    <form v-if="isSignUp">
-        <div>
-            <label>Email</label>
-            <input type="email" />
-        </div>
-        <div>
-            <label>Password</label>
-            <input type="password" />
-        </div>
-        <div>
-            <button type="submit" @click="signUp">SignUp</button>
-        </div>
-        <p>already have an account? <button type="button" @click="isSignUp = !isSignUp">Click here</button></p>
-    </form>
-    <form v-else>
-        <div>
-            <label>Email</label>
-            <input type="email" />
-        </div>
-        <div>
-            <label>Password</label>
-            <input type="password" />
-        </div>
-        <div>
-            <button type="submit" @click="signIn">signIn</button>
-        </div>
-        <p>Don't have an account? <button type="button" @click="isSignUp = !isSignUp">Click here</button></p>
-    </form>
-
+  <pre>{{ email }} {{ password }}</pre>
+  <form v-if="isSignUp">
+    <div>
+      <label>Email</label>
+      <input type="email" v-model="email" />
+    </div>
+    <div>
+      <label>Password</label>
+      <input type="password" v-model="password" />
+    </div>
+    <div>
+      <button type="submit" @click.prevent="signUp">SignUp</button>
+    </div>
+    <p>
+      already have an account?
+      <button type="button" @click="isSignUp = !isSignUp">Click here</button>
+    </p>
+  </form>
+  <form v-else>
+    <div>
+      <label>Email</label>
+      <input type="email" v-model="email" />
+    </div>
+    <div>
+      <label>Password</label>
+      <input type="password" v-model="password" />
+    </div>
+    <div>
+      <button type="submit" @click.prevent="signIn">signIn</button>
+    </div>
+    <p>
+      Don't have an account?
+      <button type="button" @click="isSignUp = !isSignUp">Click here</button>
+    </p>
+  </form>
 </template>
 
 <script setup lang="ts">
-
 const client = useSupabaseClient();
 const isSignUp = ref(false);
 
@@ -39,22 +44,22 @@ const email = ref('');
 const password = ref('');
 
 const signUp = async () => {
-    const { user, error } = await client.auth.signUp({
-        email: email.value,
-        password: password.value
-    });
-    console.log(user)
-    console.warn(error)
-}
+  console.log('signup!!');
+  const { user, error } = await client.auth.signUp({
+    email: email.value,
+    password: password.value,
+  });
+  console.log(user);
+  console.warn(error);
+};
 const signIn = async () => {
-    const { user, error } = await client.auth.signInWithPassword({
-        email: email.value,
-        password: password.value
-    });
-    console.log(user)
-    console.warn(error)
-}
-
+  const { user, error } = await client.auth.signInWithPassword({
+    email: email.value,
+    password: password.value,
+  });
+  console.log(user);
+  console.warn(error);
+};
 
 /* watchEffect(() => {
     if (user.value) navigateTo('/dashboard')
